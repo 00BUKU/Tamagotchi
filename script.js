bde3efc9e86f1db9db4e5f3f722bf175bf03f4ce
00buku <clydescottwilliamsjr@gmail.com>
2022-11-13 13:19:40 -0600

    Add queryselectors on buttons and values, fix html vals.

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ const eatButton = document.querySelector("#eatBtn");
 eatButton.addEventListener("click", () => {
     console.log("WORKS!");
 })
+
+const sleepButton = document.querySelector("#sleepBtn");
+
+sleepButton.addEventListener("click", () => {
+    console.log("WORKS!");
+})
+const playButton = document.querySelector("#playBtn");
+
+    playButton.addEventListener("click", () => {
+        console.log("WORKS!");
+})
+
