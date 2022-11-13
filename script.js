8bea12ec3255b6cd0784fcf7ea09ffcd85a0abda
00buku <clydescottwilliamsjr@gmail.com>
2022-11-13 13:52:45 -0600

    Finally add start button and query lol

--- script.js
+++ script.js
@@ -1,41 +1,47 @@
+const startBtn = document.querySelector("#startBtn");
+
+startBtn.addEventListener("click", () => {
+    console.log("start works");
+});
+
 const eatButton = document.querySelector("#eatBtn");
 
 eatButton.addEventListener("click", () => {
-    console.log("WORKS!");
+    
 })
 
 const sleepButton = document.querySelector("#sleepBtn");
 
 sleepButton.addEventListener("click", () => {
-    console.log("WORKS!");
+    
 })
 const playButton = document.querySelector("#playBtn");
 
     playButton.addEventListener("click", () => {
-        console.log("WORKS!");
+        
 })
 
 
 const eatVal = document.querySelector(".eatVal");
 
 eatVal.addEventListener("click", () => {
-    console.log("WORKS!");
+    
 })
 
 const sleepVal = document.querySelector(".sleepVal");
 
 sleepVal.addEventListener("click", () => {
-    console.log("WORKS!");
+    
 })
 
 const playVal = document.querySelector(".playVal");
 
 playVal.addEventListener("click", () => {
-    console.log("WORKS!");
+    
 })
 
 let ageVal = document.querySelector("#ageVal");
 
 ageVal.addEventListener("click" , () => {
-    console.log("age works");
+    
 })
